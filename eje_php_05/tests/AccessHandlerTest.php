9263387a257f1720034127595f9842f102ae05df
David M. Rivero G <davidmriverog@gmail.com>
2017-03-22 17:55:37 -0400

    implementacion incial de mockery

--- eje_php_05/tests/AccessHandlerTest.php
+++ eje_php_05/tests/AccessHandlerTest.php
@@ -1,17 +1,13 @@
 <?php
 
-use Styde\Authenticator;
 use Styde\AccessHandler;
-use Styde\SessionManager;
-use Styde\SessionFileDriver;
-use Styde\SessionArrayDriver;
-use Styde\Stubs\AuthenticatorStub;
+use Styde\Auth\AuthenticatorInterface;
 
 class AccessHandlerTest extends PHPUnit_Framework_TestCase
 {
     public function test_grant_access()
     {
-        $auth = new AuthenticatorStub();
+        $auth = new Mockery::mock(AuthenticatorInterface::class);
 
         $access = new AccessHandler($auth);
 
