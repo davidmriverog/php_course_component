e3bdffcd1badf5144fd55d70dc3134920011d22f
David M. Rivero G <davidmriverog@gmail.com>
2017-04-21 20:09:14 -0400

    master - inicio del ejercicio nro. 9

---
+++ eje_php_09/src/Container.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Styde;
+
+class Container
+{
+    protected static $container;
+
+    protected $shared = array();
+
+    public static function getInstance()
+    {
+        if(static::$container === null)
+            static::$container = new Container;
+
+        return static::$container;
+    }
+
+    public static function setContainer(Container $container)
+    {
+        static::$container = $container;
+    }
+
+    public static function clearContainer()
+    {
+        static::$container = null;
+    }
+
+    public function session()
+    {
+        if(isset($this->shared['session']))
+            return $this->shared['session'];
+
+        $data = [
+            'user_data'=>[
+                'name'=>'David Rivero',
+                'rol'=>'students'
+            ]
+        ];
+
+        $driver = new SessionArrayDriver($data);
+
+        return $this->shared['session'] = new SessionManager($driver);
+    }
+
+    public function auth()
+    {
+        if(isset($this->shared['auth']))
+            return $this->shared['auth'];
+
+        return $this->shared['auth'] = new Authenticator($this->session());
+    }
+
+    public function access()
+    {
+        if(isset($this->shared['access']))
+            return $this->shared['access'];
+
+        return $this->shared['access'] = new AccessHandler($this->auth());
+    }
+
+}
